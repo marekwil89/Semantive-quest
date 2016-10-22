5ce6092411e554aceda6eefced8cdaf8545330da
marekwil89 <marekwil89@gmail.com>
2016-10-22 09:30:18 +0200

    add func and form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,39 @@
 angular.module('app', [])
 
-.controller('appCtrl', function($scope){
+.controller('appCtrl', function($scope, rectangleService){
+  $scope.rectangles = rectangleService.ractangleList;
+  $scope.rectangle = rectangleService.model;
 
+
+  $scope.save = function(data){
+    rectangleService.addRectangle(data);
+    $scope.rectangles = rectangleService.getRectangles();
+  };
 })
+
+.service('rectangleService', function(){
+  this.ractangleList = [];
+
+  this.model = {
+    number: 22,
+    color: 'yellow',
+    size: 25       
+  };
+
+  this.addRectangle = function(data){
+    this.ractangleList.push({
+      number: data.number,
+      color: data.color,
+      size: data.size,
+      visual: {
+          'background-color': data.color,
+          'width': data.size + 'px',
+          'height': data.size + 'px'      
+        }        
+    });
+  };
+
+  this.getRectangles = function(data){
+    return this.ractangleList;
+  };
+});
