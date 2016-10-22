b355a47fdf08ee7e6411ee3422b52c81634c9f2b
marekwil89 <marekwil89@gmail.com>
2016-10-22 09:37:44 +0200

    validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,42 @@
 angular.module('app', [])
 
-.controller('appCtrl', function($scope, rectangleService){
+.controller('appCtrl', function($scope, rectangleService, validationService){
   $scope.rectangles = rectangleService.ractangleList;
   $scope.rectangle = rectangleService.model;
 
   $scope.$watch('rectangle', function(data){
     $scope.visualRectangle = rectangleService.visualRectangle(data)     
   }, true);
-  
+
   $scope.save = function(data){
-    rectangleService.addRectangle(data);
-    $scope.rectangles = rectangleService.getRectangles();
+    $scope.errors = validationService.form(data)
+    if($scope.errors.length === 0 ){
+      rectangleService.addRectangle(data);
+      $scope.rectangles = rectangleService.getRectangles();
+    }
   };
+})
+
+.service('validationService', function(){
+  this.form = function(data) {
+    var errors = [];
+    
+    if(typeof data === 'undefined' || !data.number || !data.color || !data.size) {
+      errors.push('all fields are required');
+    }
+    else {
+      if(isNaN(data.number)){
+        errors.push('its not a number')
+      }
+      if(data.number < 18 || data.number > 100){
+        errors.push('number must be between 18 and 100')
+      }
+      if(data.color === 'yellow' && data.number == 20){
+        errors.push('you can`t add rectangle with yellow color and number 20.')
+      }
+    }
+    return errors
+  }
 })
 
 .service('rectangleService', function(){
